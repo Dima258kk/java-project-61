e5ee26064dc10dafc25a2ffe362e083cf29336c9
Dima258kk <Dima2588k@yandex.ru>
2026-03-18 23:41:21 +0300

    add tasks.getByName and remove id("java"), testImplementation, tasks.test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
     id("application")
 }
@@ -12,15 +11,13 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 application {
     // Входная точка
     mainClass.set("hexlet.code.App")
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
 }
