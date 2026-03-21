32d18f0635f55e597ecb2c17f8d89451598b7957
Dima258kk <Dima2588k@yandex.ru>
2026-03-22 00:14:57 +0300

    fix plugins and sonar in  ../../build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     id("com.github.ben-manes.versions") version "0.52.0"
     id("application")
+    id("org.sonarqube") version "7.2.3.7755"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Dima258kk_java-project-61")
+        property("sonar.organization", "dima258kk")
+    }
 }
 
 group = "hexlet.code"
